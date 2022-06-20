ac0e0041c8b69915c36e1fd53d5d73178d033bc6
bogdan.grigorian <bogdan.grigorian@etnetera.cz>
2022-06-20 12:00:04 +0200

    [Feat #31] test refactoring

--- bpmn2stamp-converter/src/test/java/cz/cvut/kbss/bpmn2stamp/service/ConverterXmlFileReaderTest.java
+++ bpmn2stamp-converter/src/test/java/cz/cvut/kbss/bpmn2stamp/service/ConverterXmlFileReaderTest.java
@@ -3,15 +3,18 @@ package cz.cvut.kbss.bpmn2stamp.service;
 import cz.cvut.kbss.bpmn2stamp.converter.model.actor.ActorMappings;
 import cz.cvut.kbss.bpmn2stamp.converter.model.bpmn.org.omg.spec.bpmn._20100524.model.TDefinitions;
 import cz.cvut.kbss.bpmn2stamp.converter.model.organization.Organization;
+import cz.cvut.kbss.bpmn2stamp.converter.service.ConverterMappingService;
 import cz.cvut.kbss.bpmn2stamp.converter.service.ConverterXmlFileReader;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConverterXmlFileReaderTest {
 
-    private static final String TESTING_DATA_DIR = "src/test/java/cz/cvut/kbss/bpmn2stamp/service/data/";
+    private static final String TESTING_DATA_DIR = "src/test/resources/service/";
 
     public static final String BPMN_TEST_FILE = TESTING_DATA_DIR + "Jednani-sag.bpmn";
     public static final String ORG_STRUCTURE_TEST_FILE = TESTING_DATA_DIR + "ucl-zpracovani-informaci-o-bezpecnosti.xml";
@@ -41,4 +44,21 @@ public class ConverterXmlFileReaderTest {
         ActorMappings actorMappings = converterXmlFileReader.readActorMappings(ACTOR_MAPPING_TEST_FILE);
         assertThat(actorMappings).isNotNull();
     }
+
+    @Test
+    public void testReadActorMappingFile() {
+        ConverterMappingService converterMappingService = new ConverterMappingService(
+                "prefix-bpmn",
+                "prefix-organization-structure",
+                "prefix-pre-stamp"
+        );
+        ActorMappings actorsFromXml = converterMappingService.readActorMappingFile(new File(TESTING_DATA_DIR + "actor_mapping_1.xml"));
+        ActorMappings actorsFromConf = converterMappingService.readActorMappingFile(new File(TESTING_DATA_DIR + "actor_mapping_1.conf"));
+        assertThat(actorsFromXml).usingRecursiveComparison().isEqualTo(actorsFromConf);
+
+        actorsFromXml = converterMappingService.readActorMappingFile(new File(TESTING_DATA_DIR + "actor_mapping_2.xml"));
+        actorsFromConf = converterMappingService.readActorMappingFile(new File(TESTING_DATA_DIR + "actor_mapping_2.conf"));
+        assertThat(actorsFromXml).usingRecursiveComparison().isEqualTo(actorsFromConf);
+    }
+
 }
